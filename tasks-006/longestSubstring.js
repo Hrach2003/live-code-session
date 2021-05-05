e706413f913eebd0be59e17bd05ce2b15f8f6fc2
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-05-05 22:56:15 +0400

    fix(*): rewrite updating logic

--- tasks-006/longestSubstring.js
+++ tasks-006/longestSubstring.js
@@ -1,31 +1,45 @@
 // @ts-check
-let string =
-  "there are no two words in the english language more harmful than 'good job'.";
-
 /**
  * @param {string} str
  * @returns {string}
  */
 function subString(str) {
   let seenChar = {};
+
+  // tracking current substring indexes
   let startIndex = 0;
   let endIndex = 0;
-  let length = endIndex - startIndex;
+
+  // keeping largest range
+  let max = [startIndex, endIndex];
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
-    if (char in seenChar && char !== " ") {
-      let newLength = endIndex - startIndex;
-      if (length < newLength) {
-        endIndex = i;
-        length = newLength;
-        startIndex = seenChar[char] + 1;
-        seenChar = {};
-      }
+    // if char is space passing it
+    if (char === " ") {
+      endIndex++;
+      continue;
     }
-    if (char !== " ") seenChar[char] = i;
-    endIndex++;
+
+    if (!(char in seenChar)) {
+      endIndex++;
+      seenChar[char] = i;
+      continue;
+    }
+
+    // if we find larger range update max
+    if (endIndex - startIndex > max[1] - max[0]) {
+      max[0] = startIndex;
+      max[1] = endIndex;
+    }
+    // set the loop to continue from seenChar[char] + 1
+    startIndex = seenChar[char] + 1;
+    endIndex = i = startIndex;
+    seenChar = {};
   }
-  return str.substring(startIndex, endIndex);
+  // cutting from start index to end index
+  return str.substring(max[0], max[1]);
 }
+
+let string = "ABDEFGABEF";
 
 console.log(subString(string));
